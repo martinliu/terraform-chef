e70c056d609da690521c11934f5005bfa6ef66f1
martinliu <liuzh66@gmail.com>
2018-03-15 11:46:50 +0800

    cook

---
+++ cookbooks/workstation/recipes/setup.rb
@@ -0,0 +1,22 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+template '/etc/motd' do
+  source 'motd.erb'
+  action :create
+end
+
+service 'firewalld' do
+  action [ :stop, :disable ]
+end
+
+
