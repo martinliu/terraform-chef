28a89f267e19a376e48685c3411423f326b20bfd
martinliu <liuzh66@gmail.com>
2018-03-16 20:03:32 +0800

    增加了注释

--- aws-centos-docker/main.tf
+++ aws-centos-docker/main.tf
@@ -2,29 +2,40 @@ terraform {
   required_version = ">= 0.8, < 0.11.4"
 }
 
+# 使用 AWS  Tokyo
 provider "aws" {
   region = "ap-northeast-1"
 }
 
+# ec2 实例
 resource "aws_instance" "docker" {
-  ami                    = "ami-25bd2743"
-  instance_type          = "t2.medium"
-  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
-  key_name               = "${var.old_keypair}"
+  # centos 7 的 ami 镜像
+  ami           = "ami-25bd2743"
+  instance_type = "t2.medium"
 
+  # 使用下面所创建的 sg
+  vpc_security_group_ids = ["${aws_security_group.instance.id}"]
+
+  # 使用已有的 keypair
+  key_name = "${var.old_keypair}"
+
+  # 创建自定义数据
   user_data = <<-EOF
               #!/bin/bash
               echo "Hello, World" > index.html
               EOF
 
+  # 打标签
   tags {
     Name = "terraform-docker"
   }
 }
 
+# 创建 sg
 resource "aws_security_group" "instance" {
   name = "terraform-docker-instance"
 
+  # 入栈规则
   ingress {
     from_port   = "${var.service_port}"
     to_port     = "${var.service_port}"
@@ -39,6 +50,7 @@ resource "aws_security_group" "instance" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # 出栈规则
   egress {
     from_port   = 0
     to_port     = 0
--- aws-centos-docker/vars.tf
+++ aws-centos-docker/vars.tf
@@ -1,26 +1,11 @@
+# 应用的服务端口
 variable "service_port" {
   description = "The port the server will use for HTTP requests"
   default     = 8080
 }
 
+# ssh秘钥对名称
 variable "old_keypair" {
   description = "Use you old key pair to access this instance via ssh"
   default     = "test-tokyo"
-}
-
-variable "list_example" {
-  description = "An example of a list in Terraform"
-  type        = "list"
-  default     = [1, 2, 3]
-}
-
-variable "map_example" {
-  description = "An example of a map in Terraform"
-  type        = "map"
-
-  default = {
-    key1 = "value1"
-    key2 = "value2"
-    key3 = "value3"
-  }
 }
